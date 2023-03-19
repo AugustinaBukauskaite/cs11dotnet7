88c09e7abbe6f69fe0626e09955197c567c248da
AugustinaBukauskaite <127015075+AugustinaBukauskaite@users.noreply.github.com>
2023-03-19 19:31:32 +0000

    Add project files.

---
+++ Chapter01/AboutMyEnvironment/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine(Environment.CurrentDirectory);
+Console.WriteLine(Environment.OSVersion.VersionString);
+
---
+++ Chapter03/Operators/Operators/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+bool a = true;
+bool b = false;
+static bool DoStuff()
+{
+    WriteLine("I am doing some stuff.");
+    return true;
+}
+
+WriteLine();
+WriteLine($"a & DoStuff() = {a && DoStuff()}");
+WriteLine($"b & DoStuff() = {b && DoStuff()}");
