abcdf496e843589a6dee19da0fe61cf39cd7fe43
AugustinaBukauskaite <127015075+AugustinaBukauskaite@users.noreply.github.com>
2023-03-24 20:03:35 +0000

    Operators done

--- Chapter03/Operators/Operators/Program.cs
+++ Chapter03/Operators/Operators/Program.cs
@@ -1,12 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-bool a = true;
-bool b = false;
-static bool DoStuff()
-{
-    WriteLine("I am doing some stuff.");
-    return true;
-}
+﻿//// See https://aka.ms/new-console-template for more information
+//bool a = true;
+//bool b = false;
+//static bool DoStuff()
+//{
+//    WriteLine("I am doing some stuff.");
+//    return true;
+//}
 
-WriteLine();
-WriteLine($"a & DoStuff() = {a && DoStuff()}");
-WriteLine($"b & DoStuff() = {b && DoStuff()}");
+//WriteLine();
+//WriteLine($"a & DoStuff() = {a && DoStuff()}");
+//WriteLine($"b & DoStuff() = {b && DoStuff()}");
+
+//Console.Write("Input password: ");
+//string password = Console.ReadLine()!;
+//if (password.Length<8)
+//{
+//    WriteLine($"your password is too short.");
+//}
+//else
+//{
+//    WriteLine("Password is ok");
+//}
+
+int age = 50000;
+WriteLine($"The {nameof(age)} variable uses {sizeof(int)} bytes of memory.");
